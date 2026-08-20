20d2f4997401b5586853196023d9aa8060e42b31
shantanukuntal2519-pixel <shantanukuntal2519@gmail.com>
2026-08-20 16:53:24 +0530

    Update process.py

--- process.py
+++ process.py
@@ -42,7 +42,7 @@ if uploaded_file is not None:
                 """
                 
                 response = gemini_client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-3.6-flash',
                     contents=[prompt, image]
                 )
                 
